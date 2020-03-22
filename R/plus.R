7259d6d67cc8c4eea8df3315c2cdd14bb1b32710
ecor <emanuele.cordano@gmail.com>
2020-03-22 09:24:10 +0100

    updates

--- R/plus.R
+++ R/plus.R
@@ -1,7 +1,7 @@
 NULL
 #' Sum operations among two or more character string 
 #' 
-#' @param x,y addends
+#' @param e1,e2 addends
 #' 
 #' @export 
 # ##importFrom base '+'
@@ -23,7 +23,7 @@ NULL
 #' out <- dir+files
 #' 
 #' 
-'+' <- function (x, y) UseMethod("+")
+'+' <- function (e1, e2) UseMethod("+")
 
 
 
@@ -34,9 +34,8 @@ NULL
 #' 
 #' @aliases plus
 
-'+.default' <- function (x, y) {
-	base::.Primitive("+")(x, y)
-	NextMethod("+")
+'+.default' <- function (e1, e2) {
+	.Primitive("+")(e1, e2)
 }
 
 	NULL
@@ -46,9 +45,9 @@ NULL
 #' 
 #' @aliases plus
 
-'+.character' <- function(x, y) 
+'+.character' <- function(e1, e2) 
 	##if(length(e1) == length(e2)) {
-		paste(x, y, sep = '')
+		paste(e1, e2, sep = '')
 ##} else stop('String Vectors of Different Lengths')
 ###https://cran.r-project.org/doc/manuals/r-release/R-exts.html#Generic-functions-and-methods
 
